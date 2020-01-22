5c6fe909bffadff7e0297ba93037c95d01c447a9
ADWESH <adweshshaddie@gmail.com>
2020-01-22 05:36:23 +0300

    create a movie interface to be implemented

---
+++ src/main/java/dao/Sql2oMovieDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import models.Movie;
+
+import java.util.List;
+
+public interface Sql2oMovieDao {
+
+    //CREATE
+    void save(Movie movie);
+    //void addCategoryToMovie()
+
+    //READ
+    List<Movie> allMovies();
+    Movie findById(int id);
+    //List<Movies> getAllMoviesInACategory(int categoryId);
+
+    //UPDATE
+    void update(String title, int releaseYear, String writer, int duration, String summary, String review);
+
+    //DESTROY
+    void deleteById(int id);
+    void clearAll();
+}
